56eb7cfddf3d97da74bb9c46cdc7261943deb0ce
Antonio Ramirez <antonioramirez9614@gmail.com>
2024-07-25 09:47:41 -0500

    good enough?

---
+++ index.js
@@ -0,0 +1,23 @@
+const main = document.querySelector(`main`)
+const playerList = document.createElement(`ul`);
+main.appendChild(playerList)
+let playerInfo;
+// call the API to get our puppies and their info
+const getPlayers = async() => {
+  const result = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2406-FTB-ET-WEB-FT/players`)
+  playerInfo = await result.json();
+// ({ players } = playerInfo.data) destructure to access data
+console.log(players)
+  players.forEach((player)=> {
+    const puppy = document.createElement(`li`);
+    playerList.appendChild(puppy);
+    puppy.innerText = `${player.name}`
+  })
+}
+getPlayers();
+
+const displayPlayerDetails = (player)=>{
+player.forEach(detail => {
+  const detailDropDown = 
+})
+}
